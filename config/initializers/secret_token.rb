21af6e5e9af5602463f628cc96bfde03a0a00873
Kurt Ebrahim <kurt.ebrahim@gmail.com>
2014-05-31 12:18:59 -0400

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bouillabaisse::Application.config.secret_token = '660bde09e60c5cb874052a22393897389763e2012e1577aa035990954a2476c6321c364ebbf4b42a603efac111e3f17418bdb16cdc47a8cbf1c7931b426aca27'
